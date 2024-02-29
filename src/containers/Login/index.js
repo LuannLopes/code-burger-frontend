c0af0acccf61014473f0e98a77d979d34d08f9ed
LuannLopes <luanlopesofc@gmail.com>
2024-02-29 11:02:32 -0300

    feat: implement React Toastify in login and register

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,6 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import * as Yup from 'yup'
 
 import LoginImg from '../../assets/login-img.svg'
@@ -36,11 +37,27 @@ function Login() {
   })
 
   const onSubmit = async clientData => {
-    const response = await api.post('sessions', {
-      email: clientData.email,
-      password: clientData.password
-    })
-    console.log(response)
+    try {
+      const { status } = await api.post(
+        'sessions',
+        {
+          email: clientData.email,
+          password: clientData.password
+        },
+        {
+          validateStatus: () => true
+        }
+      )
+      if (status === 201 || status === 200) {
+        toast.success('Seja bem-vindo(a)')
+      } else if (status === 401) {
+        toast.error('Verifique seu e-mail e senha')
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      toast.error('Falha no sistema! Tente novamente')
+    }
   }
 
   return (
@@ -63,7 +80,7 @@ function Login() {
           <Input
             type="password"
             {...register('password')}
-            error={errors.email?.message}
+            error={errors.password?.message}
           />
           <ErrorMessage>{errors.password?.message}</ErrorMessage>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 
-// import Login from './containers/Login'
-import Register from './containers/Register'
+import Login from './containers/Login'
+// import Register from './containers/Register'
 import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <Register />
+    <Login />
+    <ToastContainer autoClose={2000} theme="colored" />
     <GlobalStyles />
   </>
 )
