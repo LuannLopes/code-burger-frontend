83a27bbc1494f5ad657994d08c728ee4c8b63e7f
LuannLopes <luanlopesofc@gmail.com>
2024-03-18 09:41:54 -0300

    feat: Create side menu

---
+++ src/components/SideMenuAdmin/menu-list.js
@@ -0,0 +1,12 @@
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
+
+const listLinks = [
+  {
+    id: 1,
+    label: 'Pedidos',
+    path: '/orders',
+    icon: ShoppingBagIcon
+  }
+]
+
+export default listLinks
